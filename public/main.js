a7e4f71a8216b3c6015869ddeca692d5f19e0aa9
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-14 19:14:09 +0900

    Revert "Fix: error while ServiceWorker registration"
    This reverts commit 38b771b2

--- public/main.js
+++ public/main.js
@@ -119,7 +119,7 @@ $(function() {
             //     }, 5000);
             // }
 
-            navigator.serviceWorker.register('assets/js/service-worker.js');
+            navigator.serviceWorker.register('https://azure.mandora.xyz:3001/public/service-worker.js');
             const showNotification = () => {
                 Notification.requestPermission().then(r => {
                     if (r === 'granted') {
