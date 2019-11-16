7c5dcb8e0368febf180f5235b6d7ec1375cff7f5
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-17 00:52:32 +0900

    Add x-forwarded-for

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ var numUsers = 0;
 io.on('connection', (socket) => {
   var addedUser = false;
 
+  console.log(socket.handshake.headers['x-forwarded-for']);
+  console.log(socket.handshake.headers['x-forwarded-for'].split(",")[0]);
+
   // when the client emits 'new message', this listens and executes
   socket.on('new message', (data) => {
     if (data.toString() == "!help") {
